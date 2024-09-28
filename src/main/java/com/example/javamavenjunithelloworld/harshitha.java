48cedb416c7744e6c2c9e05b85b240fe98c515b4
Harshitha140 <pokkulaharshitha38@gmail.com>
2024-09-28 15:16:21 +0530

    Add Utility.java and update Harshitha.java

--- src/main/java/com/example/javamavenjunithelloworld/harshitha.java
+++ src/main/java/com/example/javamavenjunithelloworld/harshitha.java
@@ -1,5 +1,6 @@
+package com.example.javamavenjunithelloworld;
 public class Harshitha {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World!");
     }
 }
